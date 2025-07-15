81d5ab4969af260b5ceb71d1dc0e0869efbc4759
Brunobassmaster <164246302+Brunobassmaster@users.noreply.github.com>
2025-07-15 11:57:06 -0300

    Update and rename Alocacao_dinamica_2.c to exercicio2.c

--- exercicio2.c
+++ exercicio2.c
@@ -1,5 +1,5 @@
 /*2) Crie uma estrutura representando um aluno de uma disciplina. Essa estrutura deve
-conter o número de matrícula do aluno, seu nome e as notas de três provas. Escreva um
+conter o nÃºmero de matrÃ­cula do aluno, seu nome e as notas de trÃªs provas. Escreva um
 programa que mostre o tamanho em byte dessa estrutura.*/
 
 #include <stdio.h>
